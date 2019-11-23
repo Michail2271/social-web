3d244ca90028299f6d79a905631d0373c2b36dc4
Michail2271 <misha2271@icloud.com>
2019-11-23 20:42:24 +0300

    Updates

---
+++ precache-manifest.80b4eb3a0650d0207f99d3208433c5b5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b44b2b4772a268b915c626441c20fcc5",
+    "url": "/social-web/index.html"
+  },
+  {
+    "revision": "61abd27afc13ee12edba",
+    "url": "/social-web/static/css/main.c911280d.chunk.css"
+  },
+  {
+    "revision": "9f3bbe02236be7535d2d",
+    "url": "/social-web/static/js/2.022b0f2b.chunk.js"
+  },
+  {
+    "revision": "61abd27afc13ee12edba",
+    "url": "/social-web/static/js/main.3d66fcde.chunk.js"
+  },
+  {
+    "revision": "e1f8d895de119846449d",
+    "url": "/social-web/static/js/runtime-main.422cdc23.js"
+  }
+]);
