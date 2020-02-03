d6855db63e6dded51337b770e234db5210ded75e
Michail <misha2271@icloud.com>
2020-02-03 19:16:30 +0300

    REFACTORING

--- src/Redux/dialogs-reducer.js
+++ src/Redux/dialogs-reducer.js
@@ -1,5 +1,23 @@
+const UPDATE_NEW_MESSAGE_TEXT = 'UPDATE-NEW-MESSAGE-TEXT'
+const SEND_MESSAGE = 'SEND-MESSAGE'
+
+
+
 const dialogsReducer = (state,action) =>{
+
+
+    if (action.type === UPDATE_NEW_MESSAGE_TEXT) {
+      state.newMessageBody = action.body;
+    }
+    else if (action.type === SEND_MESSAGE) {
+        let body = state.newMessageBody
+        state.messages.push({id:6,message: body})
+        state.newMessageBody = '';
+    }
 
 
     return state;
 }
+
+
+export default dialogsReducer;
